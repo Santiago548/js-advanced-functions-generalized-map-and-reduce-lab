da18ea3c470d6f172ea455e82f1c05b6f2833a3d
Santiago548 <dominicbarajas548@gmail.com>
2021-03-17 14:41:05 -0600

    commit tree e3662501e5af4e7004b4b75baa90d9305e7e7266

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(arr, callback ) {
+    let r = []
+    for (let i = 0; i < arr.length; i++) {
+        r.push(callback(arr[i]))
+    }
+    return r
+}
+
+function reduce(arr, callback, start) {
+    let r = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1 
+
+    for (; i < arr.length; i++) {
+        r = callback(arr[i], r)
+    }
+    return r
+}
